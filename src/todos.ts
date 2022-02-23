5d3b23be1af9b5e82e76a0ef413fd4d0c96dfe57
Hilful <hilful25@gmail.com>
2022-02-24 01:47:15 +0600

    first commit

---
+++ src/todos.ts
@@ -0,0 +1,20 @@
+export const data = [
+  {
+    id: "0",
+    title: "Task 1",
+    task: "Pick your Fruits",
+    isCompleted: false,
+  },
+  {
+    id: "1",
+    title: "Task 2",
+    task: "Go for Shopping",
+    isCompleted: false,
+  },
+  {
+    id: "2",
+    title: "Task 2",
+    task: "Pick your Kid from school",
+    isCompleted: false,
+  },
+];
